fixed latent size calculation and assignment of output padding

TransposeCNN.py
@@ -19,7 +19,7 @@ class TransposeCNN(torch.nn.Module):
                 torch.nn.Dropout(dropout)
             )
             if output_padding:
-                out_pad = output_padding[idx+1] # the plus one is to offset the padding. padding remainders are generated from the input layers of the conv, but need to be applied to the output of the transpose conv
+                out_pad = output_padding[idx] # the plus one is to offset the padding. padding remainders are generated from the input layers of the conv, but need to be applied to the output of the transpose conv
             else:
                 out_pad = 0
             l.append(

autoencoders/ConvAutoencoder.py
@@ -77,7 +77,8 @@ class ConvAutoencoder(torch.nn.Module):
 
     def forward(self, x):
         x = self.encode(x)
-        # print('latent:', x.shape)
+        print('latent:', x.shape)
+        # scsds
         x = self.decode(x)
         return x
 
@@ -193,14 +194,14 @@ if __name__ == '__main__':
     # channels = get_channels()[0]
 
     # TEST FORWARD AND BACKWARD PASSES
-    # train_tune(
-    #     {
-    #         **tunable_params,
-    #         'channels': get_channels()[0],
-    #         'optimiser': 'sgd',
-    #         'lr': 0.1,
-    #     } 
-    # )
+    train_tune(
+        {
+            **tunable_params,
+            'channels': get_channels()[0],
+            'optimiser': 'sgd',
+            'lr': 0.1,
+        } 
+    )
     
     result = tuner(train_tune, tunable_params)
             

autoencoders/ae_utils.py
@@ -1,5 +1,6 @@
 from torch.utils.tensorboard import SummaryWriter
 import torch
+from time import time
 
 def visualise_reconstruction(writer, originals, reconstructions, label):
 
@@ -8,8 +9,9 @@ def visualise_reconstruction(writer, originals, reconstructions, label):
 
 def sample(generator, latent_dim):
     batch_size = 16
-    z = torch.randn(batch_size, latent_dim, 1, 1)
+    z = torch.randn(batch_size, latent_dim, 5, 5)
     h = generator(z)
-    writer = SummaryWriter(log_dir='/home/ubuntu/ai-core/runs/Generated')
+    print(h.shape)
+    writer = SummaryWriter(log_dir=f'/home/ubuntu/ai-core/runs/Generated-{time()}')
     print('adding imgs')
-    writer.add_images('generated', h)
+    writer.add_images(f'Generated/Loss/Gen-{time()}', h)