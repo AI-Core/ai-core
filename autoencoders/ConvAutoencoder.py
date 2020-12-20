5c69e2781fdb50a5d59b161c81ccb38feee3cfe0
Harry Berg <harryaberg@gmail.com>
2020-12-20 17:33:19 +0000

    shows originals and reconstructions in single view on tensorboard

--- autoencoders/ConvAutoencoder.py
+++ autoencoders/ConvAutoencoder.py
@@ -156,14 +156,16 @@ class SampleReconstructionCallback(Callback):
     def on_validation_epoch_end(self, trainer, pl_module): # already in eval mode at this point
         for batch in train_loader:
             originals, _ = batch
+            originals = originals[:8]
             reconstructions = pl_module(originals)
             # x = x.view(x.shape[0], 1, 28, 28)
             # reconstructions = reconstructions.view(reconstructions.shape[0], 1, 28, 28)
+            imgs = torch.cat((originals, reconstructions), 0)
             writer = pl_module.logger.experiment
-            writer.add_images(f'originals/{pl_module.experiment_name}', originals)
-            writer.add_images(f'reconstructions/{pl_module.experiment_name}', reconstructions)
+            # writer.add_images(f'originals/{pl_module.experiment_name}', originals)
+            # writer.add_images(f'reconstructions/{pl_module.experiment_name}', reconstructions)
+            writer.add_images(f'reconstructions/{pl_module.experiment_name}', imgs)
             # visualise_reconstruction(pl, x, reconstructions, f'epoch-{trainer.current_epoch}')
-            sfsd
             break
 
 # def checkpointCallback():
