37956c869aa3a0666c5709fd46e020ef6052e3a5
Harry Berg <harryaberg@gmail.com>
2021-04-04 22:58:19 +0100

    Skips broken website and doesnt get image if it already exists

--- ai_core/datasets/shoes/shoes.py
+++ ai_core/datasets/shoes/shoes.py
@@ -22,7 +22,7 @@ img_dir = 'images'
 if not os.path.exists(img_dir):
     os.mkdir(img_dir)
 
-shoe_files = os.listdir('shoes_urls')
+shoe_files = os.listdir('shoes_urls')[1:]
 for file in shoe_files:
     print(file)
     with open(f'shoes_urls/{file}') as f:
@@ -35,7 +35,11 @@ for file in shoe_files:
         if not os.path.exists(brand_dir):
             os.mkdir(brand_dir)
 
-        id = create_id_from_url(url)
+        id = create_id_from_url(url) # TODO replace with uuid
+
+        if 'https://pumaimages.azureedge.net' in url:
+            print('skipping shit website')
+            continue
 
         if '.tif' in id:
             print('skipping .tif file')
@@ -43,6 +47,9 @@ for file in shoe_files:
 
         print(id)
         local_dest = f'{brand_dir}/{id}'
+        if os.path.exists(local_dest):
+            print('image already exists')
+            continue
         try:
             download_file(url, local_dest)
         except requests.exceptions.ConnectionError:
@@ -61,8 +68,20 @@ scds
 
 
 class Shoes():
-    def __init__(self, root_dir='.', download=False):
+    def __init__(self, root_dir='images', download=False, transform=None):
         if download:
-            requests.get('s3://')
-        self.images = imgs
+            r = requests.get('s3://') # download zip file
+            # unzip into root_dir
+        
+        self.img_fps = (f'{root_dir}/{brand_dir}/{filename}' for brand_dir in os.listdir(root_dir) for filename in os.listdir(f'{root_dir}/{brand_dir}')) # generate image filepaths
+
+    def __len__(self):
+        return len(self.img_fps)
+
+    def __getitem__(self, idx):
+        fp = self.img_fps[idx]
+        img = Image.open(fp)
+        if self.transform:
+            img = self.transform(img)
+        return img
 
