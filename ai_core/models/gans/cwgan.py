5e0b7fc58862f15cfdca16db420084feee14e3fa
Ivan Ying Xuan <ying.yingxuan@hotmail.com>
2021-04-15 05:56:46 +0200

    Created cWGAN class

--- ai_core/models/gans/cwgan.py
+++ ai_core/models/gans/cwgan.py
@@ -1,5 +1,7 @@
 import torch.nn as nn
 import torch
+from tqdm import tqdm
+
 
 class Generator(nn.Module):
     def __init__(self, z_dim, img_ch, features_g, num_classes, img_size, embed_size):
@@ -100,80 +102,105 @@ def weights_init(m):
         nn.init.constant_(m.bias.data, 0)
 
 
+def save_checkpoint(model, optimizer, filename="my_checkpoint.pth.tar"):
+    print("=> Saving checkpoint")
+    checkpoint = {
+        "state_dict": model.state_dict(),
+        "optimizer": optimizer.state_dict(),
+    }
+    torch.save(checkpoint, filename)
+
+
 class CWGAN:
-    def __init__(self):
+    def __init__(self, ngpu=1, workers=2, learning_rate=1e-4, optim=None, beta_1=0.0, beta_2=0.9,
+                 image_size=64, image_channels=3, gen_embedding=100, z_dim=100, features_g=64, features_d=64,
+                 critic_iterations=5, lambda_penalty=10):
         print('This model was provided by Ivan Ying Xuan and Gameli Ladzekpo')
-        pass
+        self.ngpu = ngpu
+        self.workers = workers
+        self.learning_rate = learning_rate
+        self.optim = optim
+        self.beta_1 = beta_1
+        self.beta_2 = beta_2
+        self.image_size = image_size
+        self.image_channels = image_channels
+        self.gen_embedding = gen_embedding
+        self.z_dim = z_dim
+        self.features_g = features_g
+        self.features_d = features_d
+        self.critic_iteration=critic_iterations
+        self.lambda_penalty = lambda_penalty
+        self.device = torch.device("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else "cpu")
 
-    def fit(self, dataloader):
-        # Decide which device we want to run on
-        ngpu = 1
-        workers = 2
-        device = torch.device("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else "cpu")
-        learning_rate = 1e-4
-        beta1 = 0.0
-        batch_size = 64
-        image_size = 64
-        img_ch = 3
-        num_classes = dataset.num_classes
-        gen_embedding = 100
-        z_dim = 100
-        num_epochs = 1000
-        features_g = 64
-        features_d = 64
-        critic_iterations = 5
-        lambda_pen = 10
-
-        netG = Generator(z_dim=z_dim, img_ch=img_ch, features_g=features_g,
-                 num_classes=num_classes, embed_size=gen_embedding,
-                 img_size = image_size).to(device)
-        netD = Discriminator(img_ch=img_ch, features_d=features_d,
-                            num_classes=num_classes, img_size=image_size).to(device)
-
+    def fit(self, dataset, epochs, batch_size, save_model=True):
+        dataloader = torch.utils.data.DataLoader(dataset,
+                                                 batch_size=batch_size,
+                                                 shuffle=True,
+                                                 num_workers=self.workers)
+        netD = Discriminator(
+            self.image_channels,
+            self.features_d,
+            dataset.num_classes,
+            self.image_size)
+        netG = Generator(
+            self.z_dim,
+            self.image_channels,
+            self.features_g,
+            dataset.num_classes,
+            self.image_size
+        )
         # Handle multi-gpu if desired
-        if (device.type == 'cuda') and (ngpu > 1):
-            netD = nn.DataParallel(netD, list(range(ngpu)))
-            netG = nn.DataParallel(netG, list(range(ngpu)))
+        if (self.device.type == 'cuda') and (self.ngpu > 1):
+            netD = nn.DataParallel(netD, list(range(self.ngpu)))
+            netG = nn.DataParallel(netG, list(range(self.ngpu)))
 
 
         netD.apply(weights_init)
         netG.apply(weights_init)
 
+        if self.optim is None:
+            self.optim = torch.optim.Adam()
         # Setup Adam optimizers for both G and D
-        optimizerD = optim.Adam(netD.parameters(), lr=learning_rate, betas=(beta1, 0.9))
-        optimizerG = optim.Adam(netG.parameters(), lr=learning_rate, betas=(beta1, 0.9))
+        optimizerD = self.optim(
+            netD.parameters(),
+            lr=self.learning_rate,
+            betas=(self.beta1, self.beta_2))
+        optimizerG = self.optim(
+            netG.parameters(),
+            lr=self.learning_rate,
+            betas=(self.beta1, self.beta_2))
 
-
-        fixed_noise = torch.randn(32, z_dim, 1, 1).to(device)
-        os.makedirs('model/cWGAN', exist_ok=True)
-        with open('model/cWGAN/image_data.json', 'w') as f:
-            json.dump(dataset_info, f)
+        # fixed_noise = torch.randn(32, z_dim, 1, 1).to(device)
+        # os.makedirs('model/cWGAN', exist_ok=True)
+        # with open('model/cWGAN/image_data.json', 'w') as f:
+        #     json.dump(dataset_info, f)
             
-        # Number of training epochs
-        writer_real = SummaryWriter(log_dir=f'logs_runs/logs_cWGAN/real')
-        writer_fake = SummaryWriter(log_dir=f'logs_runs/logs_cWGAN/fake')
-        writer_lossD = SummaryWriter(log_dir=f'logs_runs/logs_cWGAN/lossD')
-        writer_lossG = SummaryWriter(log_dir=f'logs_runs/logs_cWGAN/lossG')
-        writer_penalty = SummaryWriter(log_dir=f'logs_runs/logs_cWGAN/penalty')
-        iters = 0
-        suffix = f'lr={learning_rate}_beta={beta1}_batch={batch_size}'  
+        # # Number of training epochs
+        # writer_real = SummaryWriter(log_dir=f'logs_runs/logs_cWGAN/real')
+        # writer_fake = SummaryWriter(log_dir=f'logs_runs/logs_cWGAN/fake')
+        # writer_lossD = SummaryWriter(log_dir=f'logs_runs/logs_cWGAN/lossD')
+        # writer_lossG = SummaryWriter(log_dir=f'logs_runs/logs_cWGAN/lossG')
+        # writer_penalty = SummaryWriter(log_dir=f'logs_runs/logs_cWGAN/penalty')
+        # iters = 0
+        # suffix = f'lr={learning_rate}_beta={beta1}_batch={batch_size}'  
         print("Starting Training Loop...")
         # For each epoch
-        for epoch in range(num_epochs):
-            for i, (real, labels) in enumerate(dataloader):
-                real = real.to(device)
+        for epoch in range(epochs):
+            print(f"Epoch [{epoch+1}/{epochs}]")
+            for i, (real, labels) in enumerate(tqdm(dataloader)):
+                real = real.to(self.device)
                 cur_batch_size = real.shape[0]
-                labels = labels.to(device)
-                for _ in range(critic_iterations):
-                    noise = torch.randn(cur_batch_size, z_dim, 1, 1).to(device)
+                labels = labels.to(self.device)
+                for _ in range(self.critic_iterations):
+                    noise = torch.randn(cur_batch_size, self.z_dim, 1, 1).to(self.device)
                     fake = netG(noise, labels)
                     disc_real = netD(real, labels).reshape(-1)
                     disc_fake = netD(fake, labels).reshape(-1)
-                    gp = gradient_penalty(netD, labels, real, fake, device=device)
+                    gp = gradient_penalty(netD, labels, real, fake, device=self.device)
                     errD_real = torch.mean(disc_real)
                     errD_fake = torch.mean(disc_fake)
                     loss_disc = (-(errD_real - errD_fake)\
-                                + lambda_pen * gp)
+                                + self.lambda_penalty * gp)
                     netD.zero_grad()
                     loss_disc.backward(retain_graph=True)
                     optimizerD.step()
@@ -185,27 +212,30 @@ class CWGAN:
                 optimizerG.step()
 
                 # Output training stats
-                if iters % 25 == 0:
-                    print(
-                        f'Epoch [{epoch}/{num_epochs}] Batch {i}/{len(dataloader)} '
-                        + f'Loss D: {loss_disc:.4f}, loss G: {loss_gen:.4f}'
-                        )
-                    with torch.no_grad():
-                        fake = netG(noise, labels)
-                        img_grid_real = torchvision.utils.make_grid(
-                            real[:32], normalize=True
-                        )
-                        img_grid_fake = torchvision.utils.make_grid(
-                            fake[:32], normalize=True
-                        )
+                # if iters % 25 == 0:
+                #     print(
+                #         f'Epoch [{epoch}/{epochs}] Batch {i}/{len(dataloader)} '
+                #         + f'Loss D: {loss_disc:.4f}, loss G: {loss_gen:.4f}'
+                #         )
+                #     with torch.no_grad():
+                #         fake = netG(noise, labels)
+                #         img_grid_real = torchvision.utils.make_grid(
+                #             real[:32], normalize=True
+                #         )
+                #         img_grid_fake = torchvision.utils.make_grid(
+                #             fake[:32], normalize=True
+                #         )
 
-                        writer_real.add_image('Real', img_grid_real, global_step=iters)
-                        writer_real.add_scalar('D(x)', errD_real, global_step=iters)
-                        writer_fake.add_image('Fake', img_grid_fake, global_step=iters)
-                        writer_fake.add_scalar('D(G(z))', errD_fake, global_step=iters)
-                        writer_lossD.add_scalar('Loss_Discriminator', loss_disc.item(), global_step=iters)
-                        writer_lossG.add_scalar('Loss_Generator', loss_gen.item(), global_step=iters)
-                        writer_penalty.add_scalar('Gradient_Penalty', gp.item(), global_step=iters)
-                iters += 1
-        torch.save(netD, 'model/cWGAN/Discriminator.pt')
-        torch.save(netG, 'model/cWGAN/Generator.pt')
+                #         writer_real.add_image('Real', img_grid_real, global_step=iters)
+                #         writer_real.add_scalar('D(x)', errD_real, global_step=iters)
+                #         writer_fake.add_image('Fake', img_grid_fake, global_step=iters)
+                #         writer_fake.add_scalar('D(G(z))', errD_fake, global_step=iters)
+                #         writer_lossD.add_scalar('Loss_Discriminator', loss_disc.item(), global_step=iters)
+                #         writer_lossG.add_scalar('Loss_Generator', loss_gen.item(), global_step=iters)
+                #         writer_penalty.add_scalar('Gradient_Penalty', gp.item(), global_step=iters)
+                # iters += 1
+            if save_model:
+                save_checkpoint(netG, optimizerG, filename="generator.pt")
+                save_checkpoint(netD, optimizerD, filename="discriminator.pt")
+        # torch.save(netD, 'model/cWGAN/Discriminator.pt')
+        # torch.save(netG, 'model/cWGAN/Generator.pt')
